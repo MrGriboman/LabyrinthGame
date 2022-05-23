bca859b68cd9f8b07857a9b77cf909da08cac448
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2022-05-23 22:49:25 +1000

    Update LabyrinthGame.cpp

--- LabyrinthGameUpdated_1/LabyrinthGame.cpp
+++ LabyrinthGameUpdated_1/LabyrinthGame.cpp
@@ -1,12 +1,118 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
+#include <cmath>
+#include <queue>
 #include <utility>
 #include <algorithm>
 #include <chrono>
+#include <thread>
 #include <cstdio>
 #include <Windows.h>
 
 #include "labyrinth.h"
+
+#define PI 3.14159f
+
+std::string opposite(std::string x) {
+	if (x == "L")
+		return "R";
+	else if (x == "R")
+		return "L";
+	else if (x == "U")
+		return "D";
+	else if (x == "D")
+		return "U";
+}
+
+bool findEnd(const std::wstring map, std::string moves, int start, int end, int size, std::string& path) {
+	int pos = start;
+	for (int i = 0; i < moves.length(); ++i) {
+		if (moves[i] == 'L')
+			pos -= 1;
+		else if (moves[i] == 'R')
+			pos += 1;
+		else if (moves[i] == 'U')
+			pos -= size;
+		else if (moves[i] == 'D')
+			pos += size;
+	}
+	if (pos == end) {
+		path += moves;
+		return true;
+	}
+	return false;
+}
+
+bool valid(const std::wstring map, std::string moves, int start, int size) {
+	int pos = start;
+	for (int i = 0; i < moves.length(); ++i) {
+		if (moves[i] == 'L')
+			pos -= 1;
+		else if (moves[i] == 'R')
+			pos += 1;
+		else if (moves[i] == 'U')
+			pos -= size;
+		else if (moves[i] == 'D')
+			pos += size;
+	}
+	if (pos < 0 || pos > size * size - 1)
+		return false;
+	else if (map[pos] == '#')
+		return false;
+	return true;
+}
+
+const std::string& gameCheat(std::wstring &map, int size, int coins, std::string &path) {
+	int* dest = new int[coins]();
+	int k = 0;
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
+			if (map[size * i + j] == '$')
+				dest[k++] = size * i + j;
+
+	std::string add;
+	std::string directions[4]{ "L", "R", "U", "D" };
+
+	for (int i = 0; i < coins + 1; ++i) {
+		std::queue<std::string> q;
+		q.push("");
+		int start = (i) ? dest[i - 1] : size + 1;
+		int end = (i == coins) ? size * size - 2 : dest[i];
+		while (!findEnd(map, add, start, end, size, path)) {
+			add = q.front();
+			q.pop();
+			for (auto dir : directions) {
+				if (&add.back() == opposite(dir))
+					continue;
+				std::string put = add + dir;
+				if (valid(map, put, start, size))
+					q.push(put);
+			}
+		}
+	}
+	int spot = size + 1;
+	for (auto dir : path) {
+		int step;
+		if (dir == 'L')
+			step = -1;
+		else if (dir == 'R')
+			step = 1;
+		else if (dir == 'D')
+			step = size;
+		else if (dir == 'U')
+			step = -size;
+		spot += step;
+		if (map[spot] != '$')
+			map[spot] = '.';
+	}
+}
+
+void maintainAngle(double &fPlayerA) {
+	if (fPlayerA < 0)
+		fPlayerA = PI * (double)2.0f + fPlayerA;
+	if (fPlayerA > PI * (double)2.0f)
+		fPlayerA -= PI * (double)2.0f;
+}
 
 
 int main() {
@@ -18,55 +124,56 @@ int main() {
 	int coins = 0;
 	int coinsNeeded;
 
-	float playerX = 1.75f;			        // Player Start Position
-	float playerY = 2.f;
-	float fPlayerA = 0.0f;			        // Player Start Rotation
-	float fieldOfView = 3.14159f / 3.0f;	// Field of View
-	float depth;			                // Maximum rendering distance
-	float speed = 5.0f;			            // Walking Speed
-	float rotationSpeed = 3.75f;            // Rotation Speed
+	double playerX = 1.f;			        // Player Start Position
+	double playerY = 1.f;
+	double fPlayerA = 0.0f;			        // Player Start Rotation
+	double fieldOfView = PI / 3.0f;	        // Field of View
+	double depth;			                // Maximum rendering distance
+	double speed = 5.0f;			        // Walking Speed
+	double rotationSpeed = 3.75f;           // Rotation Speed
+
+	bool cheatMode = false;
+	std::string path;
 
 	std::cout << "1.Start\n2.Exit\n";
 	int menu;
 	std::cin >> menu;
-	switch (menu) {
-	case 1:
-		std::cout << "\033[2J\033[1;1H";
-		break;
-	case 2:
+	if (menu == 2)
 		return 0;
-		break;
-	}
 
 	std::cout << "Choose difficulty:\n1.Easy\n2.Medium\n3.Hard(endless suffering)\n";
 	int difficulty;
 	std::cin >> difficulty;
-	switch (difficulty) {
-	case 1:
-		mapHeight = 16;
-		mapWidth = 16;
+	if (difficulty == 1) {
+		mapHeight = 8;
+		mapWidth = 8;
 		depth = 16.0f;
-		coinsNeeded = 3;
-		break;
-	case 2:
+		coinsNeeded = 1;
+	}
+	else if (difficulty == 2) {
 		mapHeight = 25;
 		mapWidth = 25;
 		depth = 16.0f;
-		coinsNeeded = 5;
-		break;
-	case 3:
+		coinsNeeded = 2;
+	}
+	else if (difficulty == 3) {
 		mapHeight = 50;
 		mapWidth = 50;
 		depth = 50.0f;
-		coinsNeeded = 8;
-		break;
-	default:
-		mapHeight = 16;
-		mapWidth = 16;
-		depth = 16.0f;
 		coinsNeeded = 3;
-		break;
 	}
+	else {
+		mapHeight = 8;
+		mapWidth = 8;
+		depth = 16.0f;
+		coinsNeeded = 1;
+	}
+
+	std::cout << "Mode: ";
+	std::wstring mode;
+	std::wcin >> mode;
+	if (mode == L"uuddlrlrba")
+		cheatMode = true;
 
 	// Create Screen Buffer
 	wchar_t* screen = new wchar_t[screenWidth * screenHeight];
@@ -80,25 +187,70 @@ int main() {
 	labyrinth.genWay();
 	labyrinth.money(coinsNeeded);
 	map += labyrinth.draw();
-	map[mapWidth * mapHeight - 2] = '#';
 
 	auto tp1 = std::chrono::system_clock::now();
 	auto tp2 = std::chrono::system_clock::now();
 
+	if (cheatMode)
+		gameCheat(map, mapHeight, coinsNeeded, path);
+	map[mapWidth * mapHeight - 2] = '#';
+
 	while (true) {
 
-		// Making the movement undependent from the framerate
+		// Making the movement independent of the framerate
 		tp2 = std::chrono::system_clock::now();
-		std::chrono::duration<float> duration = tp2 - tp1;
+		std::chrono::duration<double> duration = tp2 - tp1;
 		tp1 = tp2;
-		float elapsedTime = duration.count();
+		double elapsedTime = duration.count();
+
+		if (cheatMode) {
+			path += 'D';
+			static int k = 0;			
+			char step = path[k];
+		
+			if (step == 'R') {				
+				while (fabs(fPlayerA) > 0.1) {
+					fPlayerA += (rotationSpeed) * 0.01;
+					maintainAngle(fPlayerA);					
+				}
+				playerY += 1;
+				k++;
+			}
+
+			if (step == 'L') {			
+				while (fabs(fPlayerA - PI) > 0.1) {
+					fPlayerA += (rotationSpeed) * 0.01;
+					maintainAngle(fPlayerA);					
+				}
+				playerY -= 1;
+				k++;
+			}
+
+			if (step == 'U') {
+				while (fabs(fPlayerA - ((double)3.0f * PI / (double)2.0f)) > 0.1) {
+					fPlayerA += (rotationSpeed) * 0.01;
+					maintainAngle(fPlayerA);					
+				}	
+				playerX -= 1;
+				k++;
+			}
+
+			if (step == 'D') {
+				while (fabs(fPlayerA - (PI / (double)2.0f)) > 0.1) {
+					fPlayerA += (rotationSpeed) * 0.01;
+					maintainAngle(fPlayerA);					
+				}
+				playerX += 1;
+				k++;
+			}
+			Sleep(200);
+		}
 
 		// Checking winning
 		if (playerX > mapHeight) {
 			break;
 		}
-
-		// Checking coins
+		
 		if (coins == coinsNeeded) {
 			map[mapWidth * mapHeight - 2] = ' ';
 		}
@@ -111,18 +263,18 @@ int main() {
 		}
 
 		// Handle CCW Rotation
-		if (GetAsyncKeyState('A') & 0x8000)
-			fPlayerA -= (rotationSpeed) * elapsedTime;
+		if (GetAsyncKeyState('A') & 0x8000 && !cheatMode)
+			fPlayerA -= (rotationSpeed)*elapsedTime;
 
 		// Handle CW Rotation
-		if (GetAsyncKeyState('D') & 0x8000)
-			fPlayerA += (rotationSpeed) * elapsedTime;
+		if (GetAsyncKeyState('D') & 0x8000 && !cheatMode)
+			fPlayerA += (rotationSpeed)*elapsedTime;
 
 		// Handle Forwards movement & collision
-		if (GetAsyncKeyState('W') & 0x8000) {
+		if (GetAsyncKeyState('W') & 0x8000 && !cheatMode) {
 
-			playerX += sinf(fPlayerA) * speed * elapsedTime;;
-			playerY += cosf(fPlayerA) * speed * elapsedTime;;
+			playerX += sinf(fPlayerA) * speed * elapsedTime;
+			playerY += cosf(fPlayerA) * speed * elapsedTime;
 			if (map.c_str()[(int)playerX * mapWidth + (int)playerY] == '#') {
 
 				playerX -= sinf(fPlayerA) * speed * elapsedTime;
@@ -131,10 +283,10 @@ int main() {
 		}
 
 		// Handle backwards movement & collision
-		if (GetAsyncKeyState('S') & 0x8000) {
+		if (GetAsyncKeyState('S') & 0x8000 && !cheatMode) {
 
-			playerX -= sinf(fPlayerA) * speed * elapsedTime;;
-			playerY -= cosf(fPlayerA) * speed * elapsedTime;;
+			playerX -= sinf(fPlayerA) * speed * elapsedTime;
+			playerY -= cosf(fPlayerA) * speed * elapsedTime;
 			if (map.c_str()[(int)playerX * mapWidth + (int)playerY] == '#') {
 
 				playerX += sinf(fPlayerA) * speed * elapsedTime;
@@ -142,23 +294,26 @@ int main() {
 			}
 		}
 
+		// Maintain camera angle
+		maintainAngle(fPlayerA);
+		
 		for (int x = 0; x < screenWidth; x++) {
 
 			// For each column, calculate the projected ray angle into world space
-			float fRayAngle = (fPlayerA - fieldOfView / 2.0f) + ((float)x / (float)screenWidth) * fieldOfView;
+			double fRayAngle = (fPlayerA - fieldOfView / 2.0f) + ((double)x / (double)screenWidth) * fieldOfView;
 
 			// Find distance to wall
-			float fStepSize = 0.1f;								
-			float distanceToWall = 0.0f;
+			double fStepSize = 0.1f;
+			double distanceToWall = 0.0f;
 
 			bool hitWall = false;		// Set when ray hits wall block
 			bool hitCoin = false;       // Set when ray hits a coin
 			bool boundary = false;		// Set when ray hits boundary between two wall blocks
 
-			float fEyeX = sinf(fRayAngle); // Unit vector for ray in player space
-			float fEyeY = cosf(fRayAngle);
+			double fEyeX = sinf(fRayAngle); // Unit vector for ray in player space
+			double fEyeY = cosf(fRayAngle);
 
-			// Incrementally cast ray from player, along ray angle, testing for 
+			// Incrementally cast ray from player, along ray angle, testing for
 			// intersection with a block
 			while (!hitWall && !hitCoin && distanceToWall < depth) {
 
@@ -180,8 +335,8 @@ int main() {
 						// Ray has hit wall
 						hitWall = true;
 
-						
-						std::vector<std::pair<float, float>> p;
+
+						std::vector<std::pair<double, double>> p;
 
 						// Test each corner of hit tile, storing the distance from
 						// the player, and the calculated dot product of the two rays
@@ -189,18 +344,21 @@ int main() {
 							for (int ty = 0; ty < 2; ty++) {
 
 								// Angle between the casted vector and the perfect vector to a corner
-								float vy = (float)nTestY + ty - playerY;
-								float vx = (float)nTestX + tx - playerX;
-								float d = sqrt(vx * vx + vy * vy);
-								float dot = (fEyeX * vx / d) + (fEyeY * vy / d);
+								double vy = (double)nTestY + ty - playerY;
+								double vx = (double)nTestX + tx - playerX;
+								double d = sqrt(vx * vx + vy * vy);
+								double dot = (fEyeX * vx / d) + (fEyeY * vy / d);
 								p.push_back(std::make_pair(d, dot));
 							}
 
 						// Sort Pairs from closest to farthest
-						sort(p.begin(), p.end(), [](const std::pair<float, float>& left, const std::pair<float, float>& right) {return left.first < right.first; });
+						sort(p.begin(),
+							p.end(),
+							[](const std::pair<double, double>& left, const std::pair<double, double>& right) {return left.first < right.first; }
+						);
 
 						// First two are closest
-						float bound = 0.01;
+						double bound = 0.01;
 						if (acos(p.at(0).second) < bound)
 							boundary = true;
 						if (acos(p.at(1).second) < bound)
@@ -214,10 +372,11 @@ int main() {
 			}
 
 			// Calculate distance to ceiling and floor
-			int ceiling = (float)(screenHeight / 2.0) - screenHeight / ((float)distanceToWall);
+			int ceiling = (double)(screenHeight / 2.0) - screenHeight / ((double)distanceToWall);
 			int floor = screenHeight - ceiling;
 			short shade;
 
+			//Hit coin
 			if (hitCoin)
 				shade = '$';
 
@@ -225,7 +384,7 @@ int main() {
 			if (hitWall) {
 
 				if (distanceToWall <= depth / 4.0f)
-					shade = 0x2588; // Very close	
+					shade = 0x2588; // Very close
 				else if (distanceToWall < depth / 3.0f)
 					shade = 0x2593;
 				else if (distanceToWall < depth / 2.0f)
@@ -246,15 +405,15 @@ int main() {
 					screen[y * screenWidth + x] = ' ';
 				else if (y > ceiling && y <= floor)
 					screen[y * screenWidth + x] = shade;
-				else { // Floor				
+				else { // Floor
 					shade = '~';
 					screen[y * screenWidth + x] = shade;
 				}
-			}			
+			}
 		}
 
 		// Display Stats
-		swprintf_s(screen, 10, L"Coins = %d", coins);		
+		swprintf_s(screen, 100, L"Coins = %d angle = %f X = %f Y = %f", coins, fPlayerA, playerX, playerY);
 
 		// Display Map
 		if (difficulty != 3) {
@@ -271,7 +430,6 @@ int main() {
 		WriteConsoleOutputCharacter(console, screen, screenWidth * screenHeight, { 0,0 }, &bytesWritten);
 	}
 
-	std::cout << "\033[2J\033[1;1H";
-	std::cout << "Congrats! You found the exit";
+	std::cout << ((!cheatMode) ? "Congrats! You found the exit" : "Congrats! A bot found the exit for you!");
 	return 0;
 }
